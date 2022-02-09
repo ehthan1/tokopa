c75c7730e1b552f3255ade3861cfed80cb02da20
portgas-x <xxuechin@gmail.com>
2022-02-10 00:54:45 +0800

    fix(冲突): 冲突解决

--- cmd/account/wire.go
+++ cmd/account/wire.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-<<<<<<< HEAD
-	"bitbucket.org/portgo/tokopa/pkg/app"
-	"bitbucket.org/portgo/tokopa/pkg/config"
-	"bitbucket.org/portgo/tokopa/pkg/database"
-	"bitbucket.org/portgo/tokopa/pkg/logger"
-	"github.com/google/wire"
-=======
 	"github.com/google/wire"
 	"github.com/portgo/tokopa/pkg/app"
 	"github.com/portgo/tokopa/pkg/config"
 	"github.com/portgo/tokopa/pkg/database"
 	"github.com/portgo/tokopa/pkg/logger"
->>>>>>> 3a2cd47 (feat(init): Init)
 )
 
 var providerSet = wire.NewSet(
--- go.mod
+++ go.mod
@@ -1,8 +1,7 @@
 module github.com/portgo/tokopa
 
 go 1.17
-<<<<<<< HEAD
-=======
+
 
 require (
 	github.com/google/wire v0.5.0 // indirect
@@ -10,4 +9,3 @@ require (
 	github.com/jinzhu/now v1.1.4 // indirect
 	gorm.io/gorm v1.22.5 // indirect
 )
->>>>>>> 3a2cd47 (feat(init): Init)
